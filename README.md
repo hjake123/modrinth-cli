34f64dea1544b9a9ff7e9f833180c9f47c072b13
Hyperlynx <hjake123@gmail.com>
2023-07-22 23:51:09 -0500

    Complete basic functionality of modrinth-get.py

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # modrinth-cli
-A simple command line utility in Python for interacting with the Modrinth API
+
+A simple command line utility in Python for interacting with the Modrinth API.
+Call syntax:
+
+```(bash)
+python modrinth-get.py <slug> <game_version (optional)>
+python modrinth-get.py -update <game_version>
+```
--- modrinth-get.py
+++ modrinth-get.py
@@ -2,6 +2,8 @@ import requests
 import json
 import urllib
 import sys
+import os
+import zipfile
 
 def choose_primary_file(version):
     for file in version['files']:
@@ -9,7 +11,33 @@ def choose_primary_file(version):
             return file
     return version['files'][0]
 
-headers = {'User-Agent': 'hyperlynx/modrinth-cli (hjake123@gmail.com)'}
+def remove_existing_mod(slug):
+    for file in os.listdir():
+        if file.endswith('.jar'):
+            modstoml = zipfile.ZipFile(file, mode='r').read("META-INF/mods.toml").decode('utf-8')
+            if 'modId="'+slug+'"' in modstoml:
+                print('Found existing mod with slug "'+slug+'", updating...')
+                os.remove(file)
+
+def get_modrinth_mod(slug, game_version, game_version_specified=False):
+    headers = {'User-Agent': 'hyperlynx/modrinth-cli (discord: hyperlynx)'}
+    responce = requests.get('https://api.modrinth.com/v2/project/' + slug + '/version', headers=headers).content
+    object = json.loads(responce.decode('utf-8'))
+
+    for version in object:
+        if not game_version_specified or game_version in version['game_versions'] :
+            if choose_primary_file(version)['filename'] in os.listdir():
+                print('Already have latest version of mod "'+slug+'"')
+                sys.exit(0)
+
+            remove_existing_mod(slug)
+            urllib.request.urlretrieve(choose_primary_file(version)['url'], choose_primary_file(version)['filename'])
+            break
+
+# Script start!
+if len(sys.argv) == 1:
+    print('Usage: python modrinth-get.py <slug> <game_version (optional)>\nOr: python modrinth-get.py -update <game_version>')
+    sys.exit(1)
 
 slug = sys.argv[1]
 game_version_specified = False
@@ -17,10 +45,15 @@ if len(sys.argv) > 2:
     game_version = sys.argv[2]
     game_version_specified = True
 
-responce = requests.get('https://api.modrinth.com/v2/project/' + slug + '/version', headers=headers).content
-object = json.loads(responce.decode('utf-8'))
+if slug == '-update' or slug == '-u':
+    if not game_version_specified:
+        print('Usage: python modrinth-get.py -update <game_version>')
+        sys.exit(1)
+    for file in os.listdir():
+        if file.endswith('.jar'):
+            modstoml = zipfile.ZipFile(file, mode='r').read("META-INF/mods.toml").decode('utf-8')
+            slug = modstoml.split('modId="')[1].split('"')[0]
+            get_modrinth_mod(slug, game_version, True)
+    sys.exit(0)
 
-for version in object:
-    if not game_version_specified or game_version in version['game_versions'] :
-        urllib.request.urlretrieve(choose_primary_file(version)['url'], choose_primary_file(version)['filename'])
-        break
+get_modrinth_mod(slug, game_version, game_version_specified)
